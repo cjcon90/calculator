354729ddba2a9d81993c04e058214e6b5a736317
Ciaran Concannon <cjcon90@protonmail.com>
2020-09-25 17:47:18 +0100

    added operator functionality

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,44 @@ for (let i = 0; i < buttons.length; i++) {
     console.log(target);
   });
 }
+
+const calculator = new Vue({
+  el: "#calc",
+  data: {
+    currentValue: "",
+    oldValue: [],
+    log: [],
+    firstOp: null,
+    secondOp: null,
+  },
+  methods: {
+    addValue(e) {
+      if (!this.currentValue && e === 0) return;
+      this.currentValue = this.currentValue + e;
+    },
+    operate(e) {
+      if (!this.currentValue) return;
+      this.oldValue.push(+this.currentValue);
+      this.currentValue = "";
+      if (!this.firstOp) {
+        this.firstOp = e;
+      } else {
+        this.secondOp = this.firstOp;
+        this.firstOp = e;
+        let sum = `${this.oldValue[this.oldValue.length - 2]} ${
+          this.secondOp
+        } ${this.oldValue[this.oldValue.length - 1]}`;
+        this.oldValue.push(eval(sum));
+        this.log.push(`${sum} = ${eval(sum)}`);
+      }
+    },
+    equals() {},
+    clear() {
+      this.currentValue = "";
+      this.oldValue = [];
+      this.log = [];
+      this.firstOp = null;
+      this.secondOp = null;
+    },
+  },
+});
